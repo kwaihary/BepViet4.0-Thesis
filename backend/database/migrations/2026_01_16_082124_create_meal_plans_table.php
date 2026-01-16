b856ea4ec11199c634544da61973e20f8bec38c8
kwaihary <130022791+kwaihary@users.noreply.github.com>
2026-01-16 15:23:36 +0700

    Update Migration

---
+++ backend/database/migrations/2026_01_16_082124_create_meal_plans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meal_plans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('recipe_id')->constrained('recipes')->onDelete('cascade');
+            
+            $table->date('date'); // Ngày ăn (2025-01-20)
+            $table->enum('meal_type', ['breakfast', 'lunch', 'dinner']); // Bữa sáng/trưa/tối
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meal_plans');
+    }
+};
