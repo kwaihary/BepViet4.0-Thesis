b359c7b51827728d747fe6dce1abac815b25f295
kwaihary <130022791+kwaihary@users.noreply.github.com>
2026-01-20 20:12:24 +0700

    Them thuoc tinh displayname

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name'); 
+            $table->string('name');
+            $table->string('display_name');
             $table->string('phone', 10)->nullable()->unique();
             $table->string('address');
             $table->string('password');
